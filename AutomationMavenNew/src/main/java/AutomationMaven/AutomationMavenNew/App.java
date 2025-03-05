9990d781ba693c675f3b1a83b8791f19b75d6687
Surbhi <Surbhi@192.168.1.134>
2025-03-05 13:01:27 +0530

    main

---
+++ AutomationMavenNew/src/main/java/AutomationMaven/AutomationMavenNew/App.java
@@ -0,0 +1,13 @@
+package AutomationMaven.AutomationMavenNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
